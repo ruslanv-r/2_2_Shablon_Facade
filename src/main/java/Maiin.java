c7d79d87988af6c9d2ce5eb870a153ea2b5a96de
ruslanv-r <rvregi@gmail.com>
2022-04-16 14:09:27 +0900

    Initial commit

---
+++ src/main/java/Maiin.java
@@ -0,0 +1,7 @@
+public class Maiin {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.mult("1011", "1000"));
+        System.out.println(bins.sum("1011", "1000"));
+    }
+}
